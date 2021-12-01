37132789c1950bfa2923c6dcfd2d7e3de1b5ad7a
Phyo Min Htun <phyominhtun1990@gmail.com>
2021-12-01 13:03:29 +0630

    first commit

--- main.tf
+++ main.tf
@@ -248,7 +248,7 @@ resource "aws_codebuild_project" "elasticbeanstalk_build" {
   queued_timeout = 480
   service_role   = aws_iam_role.codebuild.arn
   artifacts {
-    type                   = "CODEPIPELINE"
+    type = "CODEPIPELINE"
   }
 
   environment {
